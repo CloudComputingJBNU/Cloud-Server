90167090b82a4a1f4863b1b6340b37c60be07d24
DodoJBNU <dh723@jbnu.ac.kr>
2023-11-30 21:43:47 +0900

    Feat : 회원가입시, user table AWS RDS에 연동하여 insert.

---
+++ app.js
@@ -0,0 +1,20 @@
+const PORT = 3000;
+
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//router
+
+const user_add = require("./src/routes/user_add");
+
+app.use("/user/add", user_add);
+
+app.listen(PORT, () => {
+  console.log("Server started on port 3000");
+});
+
+module.exports = app;
---
+++ src/routes/user_add.js
@@ -0,0 +1,39 @@
+"use strict";
+const db = require("../config/db");
+
+const multer = require("multer");
+const express = require("express");
+const router = express.Router();
+
+router.post("/", (req, res) => {
+  let sql;
+
+  let user_id = req.body.user_id;
+  let user_email = req.body.user_email;
+  let user_pw = req.body.user_pw;
+  let user_nickname = req.body.user_nickname;
+  let params;
+
+  sql = "INSERT INTO user(id, email, pw, nickname) VALUES(?, ?, ?, ?);";
+  params = [user_id, user_email, user_pw, user_nickname];
+
+  db.query(sql, params, (err, rows, fields) => {
+    let resultCode = 404;
+    let message = "에러가 발생했습니다.";
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("insert success");
+      resultCode = 200;
+      message = "회원가입에 성공했습니다.";
+    }
+    console.log(resultCode, "+", message);
+
+    res.json({
+      code: resultCode,
+      message: message,
+    });
+  });
+});
+
+module.exports = router;
