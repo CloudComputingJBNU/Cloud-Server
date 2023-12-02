349fbb64b2e7291b746a0bc70383da0aee3dc950
DodoJBNU <dh723@jbnu.ac.kr>
2023-12-02 11:38:06 +0900

    Feat : user_get 가져오기 코드

---
+++ src/routes/user_get.js
@@ -0,0 +1,49 @@
+"use strict";
+const db = require("../config/db");
+
+const multer = require("multer");
+const express = require("express");
+const router = express.Router();
+
+router.post("/", (req, res) => {
+  let sql;
+
+  let u_id = req.body.u_id;
+  let params;
+
+  let email, nickname, info;
+  console.log(email, nickname);
+
+  sql = "SELECT email, nickname, info FROM user where id = ?;";
+
+  params = [u_id];
+
+  db.query(sql, params, (err, rows, fields) => {
+    let resultCode = 404;
+    email = "";
+    nickname = "";
+    info = "";
+    let message = "이메일, 별명 에러가 발생했습니다.";
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("insert success");
+      resultCode = 200;
+      message = "이메일, 별명에 성공했습니다.";
+
+      nickname = rows[0].nickname;
+      email = rows[0].email;
+      info = rows[0].info;
+    }
+    console.log(resultCode, message);
+
+    res.json({
+      code: resultCode,
+      message: message,
+      email: email,
+      nickname: nickname,
+      info: info,
+    });
+  });
+});
+module.exports = router;
