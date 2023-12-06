20d7ebe5c3766feca0616a0396e4a9087402afb0
DodoJBNU <dh723@jbnu.ac.kr>
2023-12-06 10:52:12 +0900

    Feat : 전체 게시글 가져오기

--- src/routes/postlist_get.js
+++ src/routes/postlist_get.js
@@ -11,33 +11,57 @@ router.post("/", (req, res) => {
 
   let category_label = req.body.category_label;
 
-  let post_id, nickname, title, img;
-
   let posts = [];
 
-  sql =
-    "SELECT post.id, user.nickname, post.title, post.img FROM post JOIN user ON post.user_id = user.id WHERE post.category_label = ?;";
-  // post_id, nickname, title, img 가져오기
-  params = [category_label];
+  if (category_label === "전체 게시글") {
+    // 전체 게시글 가져오기
+    sql =
+      "SELECT post.id, user.nickname, post.title, post.img FROM post JOIN user ON post.user_id = user.id;";
+    params = [];
 
-  db.query(sql, params, (err, rows, fields) => {
-    let resultCode = 404;
-    let message = "post_id, title, img 가져오기 실패";
-    if (err) {
-      console.log(err);
-    } else {
-      console.log("post_id, title, img 가져오기 성공");
-      resultCode = 200;
-      message = "post_id, nickname, title, img 가져오기 성공";
-      posts = rows;
-    }
-    console.log(posts);
+    db.query(sql, params, (err, rows, fields) => {
+      let resultCode = 404;
+      let message = "post_id, title, img 가져오기 실패";
 
-    res.json({
-      code: resultCode,
-      message: message,
-      posts: posts,
+      if (err) {
+        console.log(err);
+      } else {
+        console.log("post_id, title, img 가져오기 성공");
+        resultCode = 200;
+        message = "post_id, nickname, title, img 가져오기 성공";
+        posts = rows;
+      }
+      res.json({
+        code: resultCode,
+        message: message,
+        posts: posts,
+      });
     });
-  });
+  } else {
+    sql =
+      "SELECT post.id, user.nickname, post.title, post.img FROM post JOIN user ON post.user_id = user.id WHERE post.category_label = ?;";
+    // post_id, nickname, title, img 가져오기
+    params = [category_label];
+
+    db.query(sql, params, (err, rows, fields) => {
+      let resultCode = 404;
+      let message = "post_id, title, img 가져오기 실패";
+      if (err) {
+        console.log(err);
+      } else {
+        console.log("post_id, title, img 가져오기 성공");
+        resultCode = 200;
+        message = "post_id, nickname, title, img 가져오기 성공";
+        posts = rows;
+      }
+      console.log(posts);
+
+      res.json({
+        code: resultCode,
+        message: message,
+        posts: posts,
+      });
+    });
+  }
 });
 module.exports = router;
